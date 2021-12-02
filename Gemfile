06afe301f68286a3af5d6ea8151a171419fce8ed
Saenko Maksim <mm.slim.one@gmail.com>
2021-12-02 21:55:11 +0200

    added dry container

--- Gemfile
+++ Gemfile
@@ -62,6 +62,8 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
+gem 'aasm'
+gem 'dry-container'
 gem 'enumerize'
 gem 'octokit', '~> 4.0'
 gem 'omniauth-github', github: 'omniauth/omniauth-github', branch: 'master'
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     match 'auth/:provider/callback', to: 'auth#callback', via: %i[get post], as: :callback_auth
+    match 'auth/failure', to: 'auth#failure', via: %i[get post], as: :failure_auth
 
     resource :session, only: :destroy
     resources :repositories
