f2c207924fa7be5e6fc1541ec07d15f5d34f74d6
Saenko Maksim <mm.slim.one@gmail.com>
2021-12-14 18:37:23 +0200

    test

--- app/controllers/web/repositories_controller.rb
+++ app/controllers/web/repositories_controller.rb
@@ -14,8 +14,14 @@ module Web
       @repository = Repository.new
       authorize @repository
 
-      client = Octokit::Client.new(access_token: current_user.token, per_page: 100)
-      @repos = client.repos.filter { |repo| AVAILABLE_LANGUAGES.include? repo.language.downcase }
+      client = Octokit::Client.new(
+        access_token: current_user.token,
+        client_id: ENV['GITHUB_CLIENT_ID'],
+        client_secret: ENV['GITHUB_CLIENT_SECRET']
+      )
+
+      # @repos = client.repos.filter { |repo| AVAILABLE_LANGUAGES.include? repo.language.downcase }
+      @repos = client.repos
     end
 
     def show
@@ -30,7 +36,12 @@ module Web
         redirect_to new_repository_path, alert: t('.empty_github_id') and return
       end
 
-      client = Octokit::Client.new(access_token: current_user.token)
+      client = Octokit::Client.new(
+        access_token: current_user.token,
+        client_id: ENV['GITHUB_CLIENT_ID'],
+        client_secret: ENV['GITHUB_CLIENT_SECRET']
+      )
+
       repo = client.repo(repository_params[:github_id].to_i)
 
       @repository = current_user.repositories.find_or_initialize_by(github_id: repository_params[:github_id])
@@ -41,12 +52,24 @@ module Web
       @repository.repo_created_at = repo[:created_at]
       @repository.repo_updated_at = repo[:updated_at]
 
-      if @repository.save
-        redirect_to @repository, notice: t('.success')
-      else
-        flash[:alert] = t('.failure')
-        render :new
-      end
+      client.create_hook(
+        @repository.github_id,
+        'web',
+        { url: "#{ENV['BASE_URL']}/api/checks", content_type: 'json' }
+      )
+      redirect_to @repository, notice: t('.success')
+
+      # if @repository.save
+      # client.create_hook(
+      #   @repository.full_name,
+      #   'web',
+      #   { url: "#{ENV['BASE_URL']}/api/checks", content_type: 'json' }
+      # )
+      #   redirect_to @repository, notice: t('.success')
+      # else
+      #   flash[:alert] = t('.failure')
+      #   render :new
+      # end
     end
 
     private
