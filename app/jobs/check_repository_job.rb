2ce677f3365a282cd1a757880c97ae6b4fdf2867
Saenko Maksim <mm.slim.one@gmail.com>
2022-01-11 22:58:08 +0200

    debug

--- app/jobs/check_repository_job.rb
+++ app/jobs/check_repository_job.rb
@@ -4,6 +4,9 @@ class CheckRepositoryJob < ApplicationJob
   queue_as :repository
 
   def perform(check)
+    pp '------------------------------------------'
+    pp check
+    pp '------------------------------------------'
     CheckerService.new.run(check.id)
   end
 end
