cc562f267253447783fda73bef59f60491e2665b
Saenko Maksim <mm.slim.one@gmail.com>
2022-01-19 13:13:28 +0200

    debug

--- app/jobs/update_info_repository_job.rb
+++ app/jobs/update_info_repository_job.rb
@@ -4,6 +4,9 @@ class UpdateInfoRepositoryJob < ApplicationJob
   queue_as :github
 
   def perform(repo)
+    pp '------------------------------------------'
+    pp repo
+    pp '------------------------------------------'
     RepositoryService.new.update_info(repo.id)
 
     GithubHookJob.perform_later(repo.id)
