d992326162402b3fe466f3e5255072f94b78d955
Saenko Maksim <mm.slim.one@gmail.com>
2022-01-28 18:02:51 +0200

    removed unnecessary files

--- test/controllers/web/repositories/checks_controller_test.rb
+++ test/controllers/web/repositories/checks_controller_test.rb
@@ -8,12 +8,6 @@ class Web::Repositories::ChecksControllerTest < ActionDispatch::IntegrationTest
     @check = repository_checks(:passed)
   end
 
-  # test 'GET #show (guest must be redirected)' do
-  #   get repository_check_path(@repository, @check)
-
-  #   assert_redirected_to root_path
-  # end
-
   test 'GET #show (signed in user can see repository`s check)' do
     sign_in_as_with_github(:one)
 
@@ -21,20 +15,6 @@ class Web::Repositories::ChecksControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
   end
-
-  # test 'POST #create (guest must be redirected)' do
-  #   post repository_checks_path(@repository)
-
-  #   assert_redirected_to root_path
-  # end
-
-  # test 'POST #create (the user cannot check repository of other users)' do
-  #   sign_in_as_with_github(:two)
-
-  #   post repository_checks_path(@repository)
-
-  #   assert_redirected_to root_path
-  # end
 
   test 'POST #create (signed in user can see repository`s check)' do
     sign_in_as_with_github(:one)
--- test/models/repository/check/issue_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class Repository::Check::IssueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
